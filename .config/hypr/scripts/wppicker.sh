dad3fb90e1b05c9fa611dac2dde430ad87e1295f
jaidev7823 <jaidev7823@gmail.com>
2026-03-12 19:32:13 +0530

    make it default

--- .config/hypr/scripts/wppicker.sh
+++ .config/hypr/scripts/wppicker.sh
@@ -3,9 +3,6 @@
 # === CONFIG ===
 WALLPAPER_DIR="$HOME/work/hypr-rice/wallpapers/"
 SYMLINK_PATH="$HOME/.config/hypr/current_wallpaper"
-TMP_DIR="/tmp/rofi-colors"
-
-mkdir -p "$TMP_DIR"
 
 cd "$WALLPAPER_DIR" || exit 1
 IFS=$'\n'
@@ -20,27 +17,8 @@ SELECTED_WALL=$(
 [ -z "$SELECTED_WALL" ] && exit 1
 SELECTED_PATH="$WALLPAPER_DIR/$SELECTED_WALL"
 
-# === EXTRACT COLORS ===
-COLORS=$(magick "$SELECTED_PATH" \
-  -resize 80x80 \
-  -colors 4 \
-  -unique-colors txt:- | awk -F'[# ]' '/#/ {print "#" $2}')
-
-# === GENERATE COLOR ICONS ===
-MENU=""
-for c in $COLORS; do
-  ICON="$TMP_DIR/${c#\#}.png"
-  magick -size 64x64 xc:"$c" "$ICON"
-  MENU+="$c\0icon\x1f$ICON\n"
-done
-
-# === ROFI COLOR PICKER ===
-COLOR=$(printf "$MENU" | rofi -dmenu -p "Color")
-
-[ -z "$COLOR" ] && exit 1
-
-# === RUN MATUGEN ===
-printf "%s\n" "$COLOR" | matugen image "$SELECTED_PATH"
+# === RUN MATUGEN (auto choose first color) ===
+printf "1\n" | matugen image "$SELECTED_PATH"
 
 # === SET WALLPAPER ===
 swww img "$SELECTED_PATH" --transition-type any --transition-fps 60
